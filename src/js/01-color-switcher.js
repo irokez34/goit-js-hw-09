fddf86745f7c8d3e44eb0d16681afedf9b38bfa0
irokez34 <jhmdaurel@gmail.com>
2023-10-22 20:54:10 +0300

    new-code

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -7,12 +7,19 @@ const start = document.querySelector( "[data-start]" );
 const stop = document.querySelector('[data-stop]');
 let timeId = null;
 
+ function setAttrib (elem,attrib,status)  {
+    elem.setAttribute(attrib,status);
+}
+function removeAttrib (elem,attrib) {
+elem.removeAttribute(attrib);
+}
+
 start.addEventListener('click',handlerStart);
 
 function handlerStart (evt){
     evt.preventDefault();
-    start.setAttribute('disabled','true');
-    stop.removeAttribute('disabled');
+    setAttrib (start,'disabled','true');
+    removeAttrib(stop,'disabled');
     timeId = setInterval(()=> {
         body.style.backgroundColor  = getRandomHexColor();
     },1000);
@@ -22,8 +29,8 @@ stop.addEventListener('click', handlerStop)
 
 
 function handlerStop(evt) {
-    start.removeAttribute('disabled');
+    
+    setAttrib (stop,'disabled','true');
+    removeAttrib(start,'disabled');
     clearInterval(timeId);
-    stop.setAttribute('disabled','true');
-
 }

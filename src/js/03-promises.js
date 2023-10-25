aec90992ecb3781e951415fb36ec44cd3304ee12
irokez34 <jhmdaurel@gmail.com>
2023-10-25 20:45:16 +0300

    new-code

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import Notiflix from 'notiflix';
+
+
+
+const form = document.querySelector('.form');
+const amountInput = form.elements['amount'];
+const firstDelay = form.elements['delay'];
+const stepDelay = form.elements['step'];
+form.addEventListener('submit',handlerSubmit);
+
+function handlerSubmit (evt) {
+  evt.preventDefault();
+  let delay = Number(firstDelay.value);
+  const step = Number(stepDelay.value);
+  const amount = Number(amountInput.value);
+  
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise ((res,rej)=>{
+    setTimeout(()=> {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        res({position,delay});
+      } else {
+        rej({position,delay});
+      }
+    },delay);
+  });
 }
+
+for (let i = 1; i <= amount; i++){
+  createPromise(i, delay)
+  .then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`); 
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`); 
+      });
+      delay += step;
+}
+};
+
